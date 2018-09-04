7a9da9db1de99414c39b21c56dd9cdc6c7937f13
Thien Tran <thientran1996@gmail.com>
2018-09-04 16:48:26 +0700

    f

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "hochiminh/spa/test" => "places#show"
 
-  root 'page#index'
+  root 'places#index'
   # resources :places, only: :index
 end
